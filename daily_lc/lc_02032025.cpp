c56086737ad4ffdae30709721ee1d09a84380c5d
Ritwik Singh <ritwsing@amazon.com>
2025-03-02 20:28:36 +0530

    Improve upon the readability of solution - 02032025

--- daily_lc/lc_02032025.cpp
+++ daily_lc/lc_02032025.cpp
@@ -4,41 +4,33 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
-        int n1 = nums1.size();
-        int n2 = nums2.size();
+        vector<vector<int>> result;
         int i = 0, j = 0;
-        vector<vector<int>> merged2D;
+        
+        while(i < nums1.size() && j < nums2.size()) {
+            int id1 = nums1[i][0], id2 = nums2[j][0];
+            int val1 = nums1[i][1], val2 = nums2[j][1];
 
-        while(i < n1 && j < n2) {
-            vector<int> mergedRow;
-            if(nums1[i][0] == nums2[j][0]) {
-                mergedRow = {nums1[i][0], nums1[i][1] + nums2[j][1]};
+            if(id1 == id2) {
+                result.push_back({id1, val1 + val2});
                 i++, j++;
-            } else if(nums1[i][0] < nums2[j][0]) {
-                mergedRow = {nums1[i][0], nums1[i][1]};
+            } else if(id1 < id2) {
+                result.push_back({id1, val1});
                 i++;
             } else {
-                mergedRow = {nums2[j][0], nums2[j][1]};
+                result.push_back({id2, val2});
                 j++;
             }
-
-            merged2D.push_back(mergedRow);
         }
 
-        while (i < n1) {
-            vector<int> mergedRow;
-            mergedRow = {nums1[i][0], nums1[i][1]};
-            i++;
-            merged2D.push_back(mergedRow);
+        while (i < nums1.size()) {
+            result.push_back(nums1[i++]);
         }
 
-        while (j < n2) {
-            vector<int> mergedRow;
-            mergedRow = {nums2[j][0], nums2[j][1]};
-            j++;
-            merged2D.push_back(mergedRow);
+        while (j < nums2.size()) {
+            result.push_back(nums2[j++]);
         }
 
-        return merged2D;
+        return result;
     }
 };
